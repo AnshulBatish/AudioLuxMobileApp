13a4f994e7c6788ae4d9da7923dbbf34cddb8a15
Anshul Batish <Anshulbatish@gmail.com>
2024-05-12 16:15:33 -0700

    Test commit.

---
+++ app/AddNewDevice.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import { Platform, Text, View, StyleSheet } from "react-native";
+import * as Location from "expo-location";
+import NetInfo from "@react-native-community/netinfo";
+
+export default function AddNewDevice() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [ipAddress, setIpAddress] = useState(null); // New state for SSID
+  const [isWifiConnected, setIsWifiConnected] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      console.log("Connection", state);
+      console.log("Connection type", state.type);
+      console.log("Is connected?", state.isConnected);
+      console.log("Details:", state.details);
+      if (state.type === "wifi" && state.isConnected) {
+        setIpAddress(state.details.ipAddress); // Set IP Address if connected to Wi-Fi
+      }
+    });
+
+    return () => {
+      unsubscribe(); // Cleanup
+    };
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {ipAddress ? <Text style={styles.paragraph}>Please Connect to an AudioLux Device through your WiFi Page</Text> : <Text style={styles.paragraph}>Waiting...</Text>}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  paragraph: {
+    fontFamily: "KSBold",
+    fontSize: 30,
+    alignSelf: "center",
+    textAlign: "center",
+    marginBottom: 30,
+  },
+});
